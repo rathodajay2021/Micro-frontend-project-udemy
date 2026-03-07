3632c208727a26435b445da592e96cbcd695bd34
Ajay Rathod <rathodajay2021@gmail.com>
2026-03-07 13:27:58 +0530

    feat: integrate marketing and containr with each other

--- container/src/App.js
+++ container/src/App.js
@@ -1,3 +1,12 @@
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
+
 export default () => {
-    return <h1>Hi There !</h1>
-}
+  return (
+    <>
+      <h1>Hi There !</h1>
+      <hr />
+      <MarketingApp />
+    </>
+  );
+};
